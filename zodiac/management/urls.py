87b1050032905c3ed8952dd7193e80c93edd3ecb
Hillel Arnold <helrond@hotmail.com>
2018-07-26 15:09:07 -0400

    stashing

--- zodiac/management/urls.py
+++ zodiac/management/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.SplashView.as_view(), name='dashboard'),
+    path('services/add/', views.ServicesAddView.as_view(), name='services-add'),
+    path('systems/', views.SystemsListView.as_view(), name='systems-list'),
+    path('systems/add/', views.SystemsAddView.as_view(), name='systems-add'),
 ]
