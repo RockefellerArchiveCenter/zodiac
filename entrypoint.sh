ea6d6454481f53f7a51bf6149bd13afe09b73b5b
Hillel Arnold <helrond@hotmail.com>
2018-11-12 21:53:29 -0500

    changing order of commands;

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Starting celery"
-/etc/init.d/celeryd start
-/etc/init.d/celerybeat start
-
 # Apply database migrations
 ../wait-for-it.sh db:5432 -- echo "Apply database migrations"
 python manage.py makemigrations && python manage.py migrate
@@ -16,6 +12,10 @@ python manage.py makemigrations && python manage.py migrate
 # Create initial organizations and users
 echo "Setting up applications and services"
 python manage.py shell < ../setup_services.py
+
+echo "Starting celery"
+/etc/init.d/celeryd start
+/etc/init.d/celerybeat start
 
 #Start server
 echo "Starting server"
