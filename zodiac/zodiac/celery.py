9a32502807ccf47ed67043c1624bf1f6769907e8
Hillel Arnold <helrond@hotmail.com>
2019-05-29 15:36:46 -0400

    removing unnecessary code

--- zodiac/zodiac/celery.py
+++ zodiac/zodiac/celery.py
@@ -15,7 +15,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(10.0, queue_callbacks.s())
