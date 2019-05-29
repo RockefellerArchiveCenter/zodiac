707cd2236ecdafbe2c8fd83ad0d9d39cea1051d4
Hillel Arnold <helrond@hotmail.com>
2019-05-29 13:58:50 -0400

    raise exception so we can better handle results

--- zodiac/gateway/tasks.py
+++ zodiac/gateway/tasks.py
@@ -43,4 +43,6 @@ def queue_callbacks():
 @shared_task()
 def queue_request(method, url, headers, data, files, params, service_id):
     r = method_map[method](url, headers=headers, data=data, files=files, params=params)
-    return r.json()
+    if r.status_code == 200:
+        return r.json()
+    raise Exception(r.json())
