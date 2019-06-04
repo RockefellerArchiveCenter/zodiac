e1d24d8abcfb0af47cf5f5883c44a76c91258dc9
Apache <helrond@hotmail.com>
2019-06-04 20:43:55 +0000

    debugging and updating configs for centos deploy

--- zodiac/zodiac/settings.py
+++ zodiac/zodiac/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = CF.STATIC_ROOT
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
