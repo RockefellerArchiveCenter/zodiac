da25088604d677bc002cd94cc5c3b2d3de8aac28
Hillel Arnold <helrond@hotmail.com>
2019-09-15 12:24:16 -0400

    save status on completion

--- zodiac/gateway/signals.py
+++ zodiac/gateway/signals.py
@@ -4,9 +4,6 @@ from .models import ServiceRegistry, RequestLog
 
 @task_prerun.connect
 def on_task_prerun(task_id=None, task=None, *args, **kwargs):
-
-    print("{}[{}] started".format(task, task_id))
-
     # Mark service as active
     if 'service_id' in kwargs['kwargs']:
         service = ServiceRegistry.objects.get(pk=kwargs['kwargs']['service_id'])
@@ -15,9 +12,6 @@ def on_task_prerun(task_id=None, task=None, *args, **kwargs):
 
 @task_postrun.connect
 def on_task_postrun(task_id=None, task=None, retval=None, state=None, *args, **kwargs):
-
-    print("{}[{}] finished".format(task, task_id))
-
     # Mark service as inactive
     def update_service(kwargs):
         if 'service_id' in kwargs['kwargs']:
@@ -30,10 +24,16 @@ def on_task_postrun(task_id=None, task=None, retval=None, state=None, *args, **k
     # Add result to request log
     if len(kwargs['args']) > 1:
         task_result = TaskResult.objects.get(task_id=task_id)
-        request_log = RequestLog.create(
+        task_result_status = 'Error'
+        if task_result.status == 'SUCCESS':
+            task_result_status = 'Idle'
+            if task_result.result.get('count') > 0:
+                task_result_status = 'Success'
+        request_log = RequestLog.objects.create(
             service=update_service(kwargs),
             status_code=None,
             request_url=kwargs['args'][1],
             async_result_id=task_id,
-            task_result=task_result
+            task_result=task_result,
+            task_result_status=task_result_status,
         )
