aee60c086b2d37db24e7b763fb3ac028a9a14c33
Hillel Arnold <helrond@hotmail.com>
2019-10-19 17:03:51 -0500

    refactoring signals

--- zodiac/gateway/signals.py
+++ zodiac/gateway/signals.py
@@ -4,38 +4,44 @@ from celery.signals import task_prerun, task_postrun
 from django_celery_results.models import TaskResult
 from .models import ServiceRegistry, RequestLog
 
-@task_prerun.connect
-def on_task_prerun(task_id=None, task=None, *args, **kwargs):
-    # Mark service as active
+
+def update_service_status(kwargs, status):
+    """Sets `has_active_task` property on Service"""
     if 'service_id' in kwargs['kwargs']:
         service = ServiceRegistry.objects.get(pk=kwargs['kwargs']['service_id'])
-        service.has_active_task = True
+        service.has_active_task = status
         service.save()
+        return service
+    return None
+
+
+def get_task_result_status(task_result):
+    """Returns custom category of task result status"""
+    task_result_status = 'Error'
+    if task_result.status == 'SUCCESS':
+        task_result_status = 'Idle'
+        if json.loads(task_result.result).get('count', 0) > 0:
+            task_result_status = 'Success'
+    return task_result_status
+
+
+@task_prerun.connect
+def on_task_prerun(task_id=None, task=None, *args, **kwargs):
+    """Marks service as active"""
+    update_service(kwargs, True)
+
 
 @task_postrun.connect
 def on_task_postrun(task_id=None, task=None, retval=None, state=None, *args, **kwargs):
-    # Mark service as inactive
-    def update_service(kwargs):
-        if 'service_id' in kwargs['kwargs']:
-            service = ServiceRegistry.objects.get(pk=kwargs['kwargs']['service_id'])
-            service.has_active_task = False
-            service.save()
-            return service
-        return None
-
-    # Add result to request log
+    """Marks service as inactive and saves TaskResult"""
+    service = update_service(kwargs, False)
     if len(kwargs['args']) > 1:
         task_result = TaskResult.objects.get(task_id=task_id)
-        task_result_status = 'Error'
-        if task_result.status == 'SUCCESS':
-            task_result_status = 'Idle'
-            if json.loads(task_result.result).get('count', 0) > 0:
-                task_result_status = 'Success'
         request_log = RequestLog.objects.create(
-            service=update_service(kwargs),
+            service=service,
             status_code=None,
             request_url=kwargs['args'][1],
             async_result_id=task_id,
             task_result=task_result,
-            task_result_status=task_result_status,
+            task_result_status=get_task_result_status(task_result),
         )
