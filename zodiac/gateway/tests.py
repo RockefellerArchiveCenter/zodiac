fd86a3d0a489550bf10e96d1fd36a22e8b5d3ce2
Hillel Arnold <helrond@hotmail.com>
2020-03-24 11:21:22 -0400

    use fixtures in tests

--- zodiac/gateway/tests.py
+++ zodiac/gateway/tests.py
@@ -1,77 +1,30 @@
 from django.test import Client, TestCase
 from django.urls import reverse
+from zodiac import settings
 
-from .models import Application, ServiceRegistry
-
-APPLICATIONS = [
-    {'name': 'Ursa Major', 'host': 'ursa-major-web', 'port': 8005},
-    {'name': 'Fornax', 'host': 'fornax-web', 'port': 8003},
-]
-
-SERVICES = [
-    {'name': 'Store Accessions', 'application': 'Ursa Major',
-     'description': 'Stores incoming accession data and creates associated transfer objects.',
-     'external_uri': 'store-accessions', 'service_route': 'accessions',
-     'plugin': 0, 'method': 'POST', 'callback_service': 'Ursa Major.Discover Bags', 'post_service': None, },
-    {'name': 'Discover Bags', 'application': 'Ursa Major',
-     'description': 'Checks for transfer files and, if found, moves them to storage.',
-     'external_uri': 'discover-bags/', 'service_route': 'bagdiscovery/',
-     'plugin': 0, 'method': 'POST', 'callback_service': None, 'post_service': 'Fornax.Approve Transfer', },
-    {'name': 'Approve Transfer', 'application': 'Fornax',
-     'description': 'Approves transfer in Archivematica',
-     'external_uri': 'approve-transfer/', 'service_route': 'approve/',
-     'plugin': 0, 'method': 'POST', 'callback_service': 'Fornax.Request Bag Cleanup', 'post_service': 'Ursa Major.Discover Bags', },
-    {'name': 'Request Bag Cleanup', 'application': 'Fornax',
-     'description': 'Requests deletion of processed bags from source directory.',
-     'external_uri': 'request-bag-cleanup/', 'service_route': 'request-cleanup/',
-     'plugin': 0, 'method': 'POST', 'callback_service': None, 'post_service': None, },
-]
+from .models import ServiceRegistry, TaskResult
+from .tasks import delete_successful, queue_callbacks
 
 
 class GatewayTestCase(TestCase):
+    fixtures = ['initial_data.json']
 
     def setUp(self):
         self.client = Client()
 
-    def create_applications(self):
-        print("Creating applications")
-        for application in APPLICATIONS:
-            Application.objects.create(
-                name=application['name'],
-                is_active=True,
-                app_host=application['host'],
-                app_port=application['port'],
-            )
-        self.assertEqual(len(Application.objects.all()), len(APPLICATIONS))
-
-    def create_services(self):
-        print("Creating services")
-        for service in SERVICES:
-            ServiceRegistry.objects.create(
-                name=service['name'],
-                application=Application.objects.get(name=service['application']),
-                description=service['description'],
-                external_uri=service['external_uri'],
-                service_route=service['service_route'],
-                plugin=service['plugin'],
-                is_active=True,
-                is_private=False,
-                method=service['method'],
-            )
-        for service in SERVICES:
-            object = ServiceRegistry.objects.get(name=service['name'])
-            object.callback_service = ServiceRegistry.objects.get(application__name=service['callback_service'].split('.')[0], name=service['callback_service'].split('.')[1]) if service['callback_service'] else None
-            object.post_service = ServiceRegistry.objects.get(application__name=service['post_service'].split('.')[0], name=service['post_service'].split('.')[1]) if service['post_service'] else None
-            object.save()
-        self.assertEqual(len(ServiceRegistry.objects.all()), len(SERVICES))
-
     def queue_tasks(self):
-        print("Queueing tasks")
+        queued = queue_callbacks()
+        self.assertTrue(
+            isinstance(queued, dict), "queue_callbacks() did not return JSON.")
+        self.assertTrue(
+            len(queued["detail"]["callbacks"]) <= settings.MAX_SERVICES, "Too many services were called.")
+
         for service in ServiceRegistry.objects.all():
             trigger = self.client.get(reverse('services-trigger', kwargs={'pk': service.id}))
-            self.assertEqual(trigger.status_code, 200, "Wrong HTTP response code")
+            self.assertEqual(trigger.status_code, 200, "Error triggering service: {}".format(trigger.json()))
+
+        delete_successful()
+        self.assertEqual(len(TaskResult.objects.filter(status="SUCCESS")), 0, "Not all successful Tasks were deleted.")
 
     def test_gateway(self):
-        self.create_applications()
-        self.create_services()
         self.queue_tasks()
