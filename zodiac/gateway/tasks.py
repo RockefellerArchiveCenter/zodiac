c6a390fae67d27ded16ebc4b9d5128fee8749603
Hillel Arnold <helrond@hotmail.com>
2021-10-14 17:10:36 -0400

    changing logic of queue_services, removing trigger first

--- zodiac/gateway/tasks.py
+++ zodiac/gateway/tasks.py
@@ -17,31 +17,30 @@ method_map = {
 
 
 @shared_task()
-def queue_callbacks():
-    completed = {'detail': {'callbacks': []}}
-    count = 0
+def queue_services():
+    """Queues tasks for regularly scheduled services.
+
+    The maximum number of services to trigger at once can be changed by
+    setting the `MAX_SERVICES` config value. Services which have been triggered
+    least recently are triggered first.
+    """
+    completed = {'detail': {'services': []}}
 
     for registry in ServiceRegistry.objects.filter(
             is_active=True, has_active_task=False,
-            application__is_active=True).order_by('modified_time'):
-        if registry.is_callback:
-            url = render_service_path(registry, '')
-            r = queue_request.delay(
-                'post',
-                url,
-                headers={'Content-Type': 'application/json'},
-                data=None,
-                files=None,
-                params={},
-                service_id=registry.id
-            )
-            if r:
-                completed['detail']['callbacks'].append({registry.name: r.id})
-            count += 1
-            if count >= settings.MAX_SERVICES:
-                break
-        else:
-            registry.save()
+            application__is_active=True).order_by('modified_time')[:settings.MAX_SERVICES]:
+        url = render_service_path(registry, '')
+        r = queue_request.delay(
+            'post',
+            url,
+            headers={'Content-Type': 'application/json'},
+            data=None,
+            files=None,
+            params={},
+            service_id=registry.id
+        )
+        if r:
+            completed['detail']['services'].append({registry.name: r.id})
     return completed
 
 
@@ -60,39 +59,6 @@ def queue_request(method, url, headers, data, files, params, service_id):
 
 @shared_task()
 def delete_successful():
-    TaskResult.objects.filter(status="SUCCESS",
-                              date_done__lte=timezone.now() - timezone.timedelta(hours=settings.DELETE_SUCCESSFUL_AFTER)).delete()
-
-
-@shared_task()
-def trigger_first_services():
-    """Calls Services which are not triggered by another service.
-
-    The FILTER_PARAMS variable is a list of filter params values which should
-    match one or more ServiceRegistry objects.
-    """
-
-    QUERY_PARAMS = [{"is_active": True, "application__is_active": True,
-                     "has_active_task": False,
-                     "application__name__icontains": "zorya",
-                     "name__icontains": "discover bags"}]
-
-    completed = {'detail': {'services': []}}
-    count = 0
-    for params in QUERY_PARAMS:
-        if ServiceRegistry.objects.filter(**params).exists():
-            for registry in ServiceRegistry.objects.filter(**params):
-                url = render_service_path(registry, '')
-                r = queue_request.delay(
-                    'post',
-                    url,
-                    headers={'Content-Type': 'application/json'},
-                    data=None,
-                    files=None,
-                    params={},
-                    service_id=registry.id
-                )
-                if r:
-                    completed['detail']['services'].append({registry.name: r.id})
-                count += 1
-    return completed
+    """Deletes expired success messages."""
+    expired_time = timezone.now() - timezone.timedelta(hours=settings.DELETE_SUCCESSFUL_AFTER)
+    TaskResult.objects.filter(status="SUCCESS", date_done__lte=expired_time).delete()
